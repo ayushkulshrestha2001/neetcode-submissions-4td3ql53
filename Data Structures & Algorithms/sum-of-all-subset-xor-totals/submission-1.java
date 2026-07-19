11045d14597e76f2a9dfb39c4321ef388285658c
Ayush Kulshrestha <59387075+ayushkulshrestha2001@users.noreply.github.com>
2026-07-19 21:40:31 +0530

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    int res = 0;
+    private void backtrack(int[] nums, int i, List<Integer> subset) {
+        int xorr = 0;
+        for(int num : subset) {
+            xorr = xorr^num;
+        }
+        res+=xorr;
+        for(int j=i;j<nums.length;j++) {
+            subset.add(nums[j]);
+            backtrack(nums, j+1, subset);
+            subset.remove(subset.size()-1);
+        }
+    }
+
+    public int subsetXORSum(int[] nums) {
+        backtrack(nums, 0, new ArrayList<>());
+        return res;
+    }
+}
